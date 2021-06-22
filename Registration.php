c252adc291992ae7d804fc9f74fa6f075eef2b91
shahriar-rafat <55339610+shahriar-rafat@users.noreply.github.com>
2021-06-22 10:41:54 -0700

    Create Registration.php

---
+++ Registration.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>Registration Form</title>
+</head>
+<body>
+<table style="width:30%">
+  <tr>
+    <td>Firstname</td>
+    <th><input type="text"></th>
+  </tr>
+  <tr>
+    <td>E-mail</td>
+    <th><input type="text"></th>
+  </tr>
+  <tr>
+    <td>Password</td>
+   <th> <input type="password"></th>
+  </tr>
+  <tr>
+    <td>Comment</td>
+    <th><textarea comment="comment"form="rform"></textarea></th><br>
+  </tr>
+  <tr>
+    <td>Gender</td>
+    <th><input type="radio" name="gender">Female<input type="radio" name="gender">Male<input type="radio" name="gender">Other</th>
+    
+  </tr>
+  <tr>
+    <td>Please Choose a hobby </td>
+    <th><input type="checkbox">Singing<input type="checkbox">Dancing<input type="checkbox"> Reading</th>
+  </tr>
+  <tr>
+    <td>Please choose a file  </td>
+    <th><input type="file"> </th>
+    
+  
+    
+    
+  </tr>
+  <tr>
+    <td><input type="Submit"> <input type="Reset"> </td>
+   
+  </tr>
+</table>
+</body>
+</html>
